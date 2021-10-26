0820eeccc06bba5624b864e3d8eb8000139f5cf5
Vishnu Ganta <vishnu.ganta22@gmail.com>
2021-10-26 13:05:02 -0400

    Update kadaneAlgorithm.cpp
    added code for Bruteforce method

--- kadaneAlgorithm.cpp
+++ kadaneAlgorithm.cpp
@@ -3,8 +3,23 @@
 
 
 #include <iostream>
+#include <limits.h>
 
 using namespace std;
+
+// Brute force method
+void printSubArrayOfMaxSum(int* arr, int size){
+    int maxSum = INT_MIN;
+    for(int i = 0; i < size; i++){
+        maxSum = arr[i] > maxSum ? arr[i] : maxSum;
+        int previous = arr[i];
+        for(int j = i + 1; j < size; j++){
+            previous = previous + arr[j];
+            if(previous > maxSum) maxSum = previous;
+        }
+    }
+    cout<<"Max Sum :: " << maxSum<<endl;
+}
 
 int main()
 {
@@ -14,5 +29,6 @@ int main()
     for(int i = 0; i < size; i++){
         cin>>arr[i];
     }
+    printSubArrayOfMaxSum(arr, size);
     return 0;
 }
