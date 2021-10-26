85b3b10d687298ba7af55958d4a32891906cac83
Vishnu Ganta <vishnu.ganta22@gmail.com>
2021-10-26 09:28:32 -0400

    Update kadaneAlgorithm.cpp
    added comments

--- kadaneAlgorithm.cpp
+++ kadaneAlgorithm.cpp
@@ -1,3 +1,7 @@
+// Maximum Subarray Problem
+// The maximum subarray problem is the task of finding the largest possible sum of a contiguous subarray, within a given one-dimensional array A[1…n] of numbers
+
+
 #include <iostream>
 
 using namespace std;
